c99f53aaa1f53813d9a546694524fd055a657144
kusham <kusham1998@gmail.com>
2023-01-27 00:26:20 +0530

    import routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import cors from 'cors';
-import connectDB from './mongoDB/connect';
+
+import connectDB from './mongoDB/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 
 const startServer = async () => {
@@ -17,5 +23,5 @@ const startServer = async () => {
       console.log(error);
     }
   }; 
-  
+    
   startServer(); 
